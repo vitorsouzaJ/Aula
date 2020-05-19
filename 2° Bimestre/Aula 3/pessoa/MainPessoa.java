587b4f41d39d6ba64d0a6e3e35e3a2bb363e8724
vitorsouzaJ <63322105+vitorsouzaJ@users.noreply.github.com>
2020-05-19 17:44:19 -0300

    Add files via upload

---
+++ 2° Bimestre/Aula 3/pessoa/MainPessoa.java
@@ -0,0 +1,26 @@
+
+public class MainPessoa {
+
+
+	public static void main(String[] args) {
+
+	
+	Pessoa p =  new Pessoa();
+	
+	p.setNome ("vitor souza de jesus");
+	p.setIdade(18);
+	p.setEndereco("São Bento do Sul/SC");
+	
+	
+	System.out.println("Meu nome é "+ p.getNome() + " tenho "+p.getIdade()+ " e moro em "+ p.getEndereco());
+	
+	
+	
+
+		
+		
+}
+
+}
+
+
