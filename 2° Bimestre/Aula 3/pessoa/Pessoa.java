7ea3d2a31f965f53f4fdcdd712eecf20e29e59a2
vitorsouzaJ <63322105+vitorsouzaJ@users.noreply.github.com>
2020-05-19 17:43:46 -0300

    Create Pessoa.java

---
+++ 2° Bimestre/Aula 3/pessoa/Pessoa.java
@@ -0,0 +1,41 @@
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+	private String endereco;
+	private int CEP;
+	private String nomePai;
+	private String nomeMae;
+	
+	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public void setEndereco (String endereco) {
+		this.endereco = endereco;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}	
+	
+	public String getEndereco() {
+		return endereco;	}
+	
+	
+	
+	
+	
+}
