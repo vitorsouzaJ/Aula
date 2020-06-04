ee06e3b2a6499387b3cfe4aa4982924b699df158
vitorsouzaJ <63322105+vitorsouzaJ@users.noreply.github.com>
2020-06-04 16:15:31 -0300

    Create Tamanho.java

---
+++ 2° Bimestre/Aula 06/Exercicio 2/Tamanho.java
@@ -0,0 +1,39 @@
+
+public class Tamanho {
+
+	private tipoTamanho tamanho;
+	private String nome;
+	
+	
+	
+	
+	
+
+	public Tamanho(tipoTamanho tamanho, String nome) {
+		super();
+		this.tamanho = tamanho;
+		this.nome = nome;
+		
+		
+	}
+	public tipoTamanho getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(tipoTamanho tamanho) {
+		this.tamanho = tamanho;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+
+
+
+
+
+
+}
