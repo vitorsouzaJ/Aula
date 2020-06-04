4479b9e9d2d821d037cae160f9650ff836dbd48f
vitorsouzaJ <63322105+vitorsouzaJ@users.noreply.github.com>
2020-06-04 16:13:05 -0300

    Create Meses.java

---
+++ 2° Bimestre/Aula 06/Meses.java
@@ -0,0 +1,22 @@
+
+public class Meses {
+
+	public static void main(String[]arg) {
+		
+		System.out.println(mesesEnum.JANEIRO);
+		System.out.println(mesesEnum.FEVEREIRO);
+		System.out.println(mesesEnum.AGOSTO);
+		System.out.println(mesesEnum.ABRIL );
+
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
